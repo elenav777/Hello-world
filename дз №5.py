292b5b05d034a1fed2249b80a42a1bf264c246fb
vasilya <55867923+elenav777@users.noreply.github.com>
2019-10-24 19:01:44 +0400

    Add files via upload

---
+++ дз №5.py
@@ -0,0 +1,10 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> B = 8
+>>> Kb = float(B) / float(9.77)
+>>> Mb = float(B) / float(0.0095)
+>>> print(Kb)
+0.8188331627430911
+>>> print(Mb)
+842.1052631578948
+>>> 
