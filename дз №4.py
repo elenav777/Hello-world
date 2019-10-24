efd1f4b5f72c006ab63ec6e7965b0e8b3ea233f3
vasilya <55867923+elenav777@users.noreply.github.com>
2019-10-24 18:56:24 +0400

    Add files via upload

---
+++ дз №4.py
@@ -0,0 +1,10 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> g = 500
+>>> kg = float(g) / float(1000)
+>>> t = float(g) / float(1000000)
+>>> print(kg)
+0.5
+>>> print(t)
+0.0005
+>>> 
