62e5f024edf0a3ed033ef2d456fc47295b41bbbf
vasilya <55867923+elenav777@users.noreply.github.com>
2019-10-24 18:51:55 +0400

    Add files via upload

---
+++ задание 3.py
@@ -0,0 +1,13 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> a = 12
+>>> b = 9
+>>> c = 4
+>>> p = (a+b+c) / 2.0
+>>> S = ( p*(p-a)*(p-b)*(p-c))
+>>> print(p)
+12.5
+>>> print(S)
+185.9375
+>>> 
